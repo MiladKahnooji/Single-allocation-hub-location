d18b59961997eb999279f80a72e2cd5c2979ff0d
miladkahnooji <miladkahnooji@gmail.com>
2026-09-13 18:00:30 +0330

    Use CPU-safe notebook delivery setup

--- run_setup.sh
+++ run_setup.sh
@@ -10,7 +10,11 @@ python3.12 -m venv .venv
 
 # Optional executed-notebook verification.  The notebooks write only under
 # their configured ignored output directory.
-.venv/bin/python -m jupyter nbconvert --to notebook --execute --inplace \
+.venv/bin/python -m ipykernel install --prefix "$PWD/.jupyter" \
+  --name salh312 --display-name "SALH Python 3.12"
+JUPYTER_PATH="$PWD/.jupyter/share/jupyter" .venv/bin/python -m jupyter nbconvert --to notebook --execute --inplace \
+  --ExecutePreprocessor.kernel_name=salh312 \
   --ExecutePreprocessor.timeout=600 notebooks/Single_Allocation_Hub_Location_Complete.ipynb
-.venv/bin/python -m jupyter nbconvert --to notebook --execute --inplace \
+JUPYTER_PATH="$PWD/.jupyter/share/jupyter" .venv/bin/python -m jupyter nbconvert --to notebook --execute --inplace \
+  --ExecutePreprocessor.kernel_name=salh312 \
   --ExecutePreprocessor.timeout=600 notebooks/article_presentation_figures.ipynb
